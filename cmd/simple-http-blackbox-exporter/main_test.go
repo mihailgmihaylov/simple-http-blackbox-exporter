705cb0d0ebb8d4822c4c286775cead4c21adf942
Mihail Mihaylov <mihail.mihaylov@receipt-bank.com>
2020-04-25 18:09:22 +0300

    Add unit test - test return values from HTTP.Get

--- cmd/simple-http-blackbox-exporter/main_test.go
+++ cmd/simple-http-blackbox-exporter/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/url"
+	"strconv"
 	"testing"
 )
 
@@ -17,6 +18,28 @@ func TestGetConfURLs(t *testing.T) {
 		u, err := url.Parse(httpURL)
 		if err != nil || u.Scheme == "" || u.Host == "" {
 			t.Errorf("Incorrect url scheme or host: %v or %v.", u.Scheme, u.Host)
+		}
+	}
+}
+
+func TestGetURLValues(t *testing.T) {
+	var codes = []int{200, 201, 202, 203, 204, 205, 206, 301, 302, 303, 304, 305, 306, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 500, 501, 502, 503, 504, 505, 506, 507}
+	for _, code := range codes {
+		url := "https://httpstat.us/" + strconv.Itoa(code)
+		resp, time := GetURL(url)
+		if resp == 0 && time != 0 {
+			t.Errorf(
+				"Incorrect response time - it should be 0 when response failed but it is: %v for URL: %v",
+				time, url,
+			)
+		}
+
+		if resp != 0 && resp != 1 {
+			t.Errorf("Incorrect response code received: %v but it should be 0 or 1.", time)
+		}
+
+		if time < 0 {
+			t.Errorf("Negative response time received: %v but it should be a positive value.", time)
 		}
 	}
 }
